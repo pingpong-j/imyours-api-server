95e2ff4f392b33b50deaed1f90ebc3857530a607
yoonsung-lee <yoonsung.lee@linecorp.com>
2021-02-08 03:55:39 +0900

    Enable helloworld service

---
+++ build.sbt
@@ -0,0 +1,16 @@
+name := "imyours-api-server"
+
+version := "0.1"
+
+scalaVersion := "2.13.4"
+
+val http4sVersion = "0.21.15"
+
+// Only necessary for SNAPSHOT releases
+resolvers += Resolver.sonatypeRepo("snapshots")
+
+libraryDependencies ++= Seq(
+  "org.http4s" %% "http4s-dsl" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-client" % http4sVersion
+)
---
+++ src/main/scala/com/imyours/api/server/Main.scala
@@ -0,0 +1,29 @@
+package com.imyours.api.server
+
+import cats.effect.{ExitCode, IO, IOApp, _}
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
+import org.http4s.server.blaze.BlazeServerBuilder
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+object Main extends IOApp {
+  implicit val cs: ContextShift[IO] = IO.contextShift(global)
+//  implicit val timer: Timer[IO] = IO.timer(global)
+
+  private val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name.")
+  }.orNotFound
+
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO](global)
+      .bindHttp(8080, "localhost")
+      .withHttpApp(helloWorldService)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
+}
